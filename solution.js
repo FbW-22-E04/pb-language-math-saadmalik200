ac6cda458db3468261d7610f3edb192e46c3a0a0
Saad Malik <saad_malik200@hotmail.com>
2022-07-05 15:27:19 +0200

    fixed

--- solution.js
+++ solution.js
@@ -19,9 +19,11 @@ console.log(Math.floor(3321.32321));
 console.log(Math.floor(326.76));
 console.log(Math.floor(76788.7));
 console.log(Math.floor(-9.78));
-console.log(Math.floor(43.342));
+console.log(Math.floor(28.329));
 
 // ## 3. Dice Roll!
 
-const randonInt = Math.ceil(Math.random() * 6);
-console.log(randonInt);
+// const randonInt = Math.ceil(Math.random() * 6);
+// console.log(randonInt);
+
+console.log(Math.floor(Math.random() * 6 + 1));
